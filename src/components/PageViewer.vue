1b9234c1b0339b256efddcaf8bfb2445a87dd557
ayoubbr <ayoub1benmansour@gmail.com>
2025-04-08 16:28:37 +0100

    something

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -2,11 +2,10 @@
     <div class="container">
         <h1>{{ page.pageTitle }}</h1>
         <p>{{ page.content }}</p>
-        test
     </div>
 </template>
 <script>
 export default {
     props: ['page']
 }
-</script> 
+</script>
